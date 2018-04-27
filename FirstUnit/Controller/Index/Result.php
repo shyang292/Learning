d10a50ed03d6a98f6b9ae0b0c6a5bbcda3cc6840
shyang292 <shyang292@gmail.com>
2018-04-27 05:14:53 +0000

    magento 2 simple form submit april 30

---
+++ FirstUnit/Controller/Index/Result.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Learning\FirstUnit\Controller\Index;
+
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Element\Messages;
+use Magento\Framework\View\Result\PageFactory;
+
+class Result extends Action
+{
+    /** @var PageFactory $resultPageFactory */
+    protected $resultPageFactory;
+
+    /**
+     * Result constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->resultPageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * The controller action
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $number = $this->getRequest()->getParam('number');
+
+        $resultPage = $this->resultPageFactory->create();
+
+        /** @var Messages $messageBlock */
+        $messageBlock = $resultPage->getLayout()->createBlock(
+            'Magento\Framework\View\Element\Messages',
+            'answer'
+        );
+        if (is_numeric($number)) {
+            $messageBlock->addSuccess($number . ' times 2 is ' . ($number * 2));
+        }else{
+            $messageBlock->addError('You didn\'t enter a number!');
+        }
+
+        $resultPage->getLayout()->setChild(
+            'content',
+            $messageBlock->getNameInLayout(),
+            'answer_alias'
+        );
+
+        return $resultPage;
+    }
+}
---
+++ FirstUnit/registration.php
@@ -0,0 +1,4 @@
+<?php \Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Learning_FirstUnit',
+    __DIR__
+);
